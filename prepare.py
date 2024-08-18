e6f335f13d75dc9f7a0cee7cfb94b0dfadf0c8ef
ningwebbeginner <52982115+ningwebbeginner@users.noreply.github.com>
2024-08-18 22:09:42 +0800

    Update prepare.py
    add embedding model line

--- prepare.py
+++ prepare.py
@@ -4,5 +4,7 @@ config = Config()
 # 清空之前的模型和数据
 config.initialize()
 
-# 准备模型
+# 准备对话模型
 download_and_quantize_model(config.get("model_name"), config.get("cache_path"), config.get("model_path"))
+# 准备embedding模型
+download_embedding_model(config.get("embedding_model_name"),config.get("embedding_model_path"))
