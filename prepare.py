c1f5fa24d8b3a23f567f187fc2d25b208bc2759b
ningwebbeginner <52982115+ningwebbeginner@users.noreply.github.com>
2024-08-23 14:19:37 +0800

    Update prepare.py
    add download 4bit quantized model from modelscope

--- prepare.py
+++ prepare.py
@@ -1,4 +1,4 @@
-from RAG.utils import download_and_quantize_model, Config
+from RAG.utils import download_and_quantize_model, Config, download_embedding_model, download_4bit_quantized_model
 
 config = Config()
 # 清空之前的模型和数据
@@ -6,5 +6,7 @@ config.initialize()
 
 # 准备对话模型
 download_and_quantize_model(config.get("model_name"), config.get("cache_path"), config.get("model_path"))
+# 或者直接下载已经量化好的gptq模型或awq模型，模型选择请参照模型文档
+# download_4bit_quantized_model(config.get("model_name"), config.get("cache_path"))
 # 准备embedding模型
-download_embedding_model(config.get("embedding_model_name"),config.get("embedding_model_path"))
+download_embedding_model(config.get("embedding_model_name"), config.get("embedding_model_path"))
